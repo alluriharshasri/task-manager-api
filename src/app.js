a2d39e3cf3aa0fb1142c1777852384d8bacb3a0a
alluriharshasri <alluriharshasri@gmail.com>
2025-12-29 11:57:01 +0530

    Initial Node.js API

---
+++ src/app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+app.get("/health", (req, res) => {
+  res.status(200).json({ status: "OK" });
+});
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
+
+module.exports = app;
